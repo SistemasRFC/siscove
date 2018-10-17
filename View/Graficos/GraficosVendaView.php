56e689379aaa5fd40b078566162e55cbd598209b
Rafael Freitas Carneiro <rafaelfcarneiro@gmail.com>
2018-10-17 09:29:43 -0300

    [FIX]Correção no select.

--- View/Graficos/GraficosVendaView.php
+++ View/Graficos/GraficosVendaView.php
@@ -1,7 +1,7 @@
 <?php include_once "../../View/MenuPrincipal/Cabecalho.php";?>
 <html>
 <HEAD>
-<TITLE>Estatística de Vendas</TITLE>
+    <TITLE>Estat&iacute;stica de Vendas</TITLE>
 	<meta http-equiv="Content-Type" content="text/HTML; charset=utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1 minimum-scale=1" />	        
